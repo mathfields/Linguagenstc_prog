5bc368a649ee4962c2f7d4dca0baf6cdf08b765a
mathfields <141881962+mathfields@users.noreply.github.com>
2026-08-04 09:25:37 -0300

    Add files via upload

---
+++ EX.Triangulo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	 int base, altura;
+	 float area;
+base = 10;
+altura = 10;
+area = (base*altura)/2;
+ printf("A base do triangulo é %d * %d = %f",base,altura,area);
+ 
+	return 0;
+}
