2d338c9db9788b6fc442806241ff9ea0c98a68ee
mathfields <141881962+mathfields@users.noreply.github.com>
2026-09-01 09:31:06 -0300

    Add files via upload

---
+++ Listadadeexercicio menu.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	int escolha;
+	
+	printf("Escolha o exercicio \n 1 \n 2 \n 3 \n 4 \n 5 \n 6 \n 7 \n 8 \n :");
+	
+	scanf("%d" &escolha)
+	
+	if (escolha > 8 && escolha <1){
+		printf("Escolha um numero valido");
+	}
+	
+	switch (escolha){
+		case '1':
+		
+	}
+	
+	
+	return 0;
+}
